cbc22f2403da8c09b12a6acf1399a2877bf90ba2
Hack Reactor Student <student@hackreactor.com>
2017-08-11 16:07:05 -0500

    atom is a meme

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  $(".leftEye").hover(function() {
+    $(".leftEye").html('<');
+    $(".leftEye").css("font-size", "200px");
+    $(".leftEye").css("margin-top", "340px");
+    $(".leftEye").css("margin-left", "750px");
+  });
+  $(".rightEye").hover(function() {
+    $(".rightEye").html('>');
+    $(".rightEye").css("font-size", "200px");
+    $(".rightEye").css("margin-top", "340px");
+    $(".rightEye").css("margin-left", "414px");
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
